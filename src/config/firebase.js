c45244bbd7802cbbfae9d32bc11ff6ff2db1c71d
M Nouman <rn9728959@gmail.com>
2024-11-02 19:37:22 +0500

    updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,14 +6,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPySx4KPxanY_cqvlz7rXqcF-dVqXK-cQ",
-  authDomain: "nn-lms.firebaseapp.com",
-  databaseURL: "https://nn-lms-default-rtdb.firebaseio.com",
-  projectId: "nn-lms",
-  storageBucket: "nn-lms.appspot.com",
-  messagingSenderId: "57212172692",
-  appId: "1:57212172692:web:3c6b188ccb9ba44823b781",
-  measurementId: "G-1VTXY8RX2L"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
